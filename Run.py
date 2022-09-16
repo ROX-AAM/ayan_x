6da5eedfc5ed0f7f1cfb461372068556c80ec11f
MINHᎯZ <110917582+maminhaz60@users.noreply.github.com>
2022-09-16 16:32:04 +0600

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,18 @@
+import os, platform, time
+print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
+os.system('git pull')
+try:
+    import gtts
+except IOError:
+    os.system('pip install gtts')
+def Update():
+    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '32bit':
+            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+
+            import Riaz
+        else:
+            exit('\033[1;31m[×] Device Not Support 64bit')
+Run()
